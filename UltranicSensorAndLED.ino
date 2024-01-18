1695edba5530616b9789177f1b42e67d92440947
Nishith Fernando <61730280+NFernando98@users.noreply.github.com>
2024-01-17 19:47:25 -0500

    Brightness change with respect to distance

--- UltranicSensorAndLED.ino
+++ UltranicSensorAndLED.ino
@@ -2,6 +2,8 @@
 #define echoPin 12
 #define led 11
 
+int brightness = 0;    // how bright the LED is
+
 void setup() {
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
@@ -11,6 +13,8 @@ void setup() {
 }
 
 void loop() {
+
+
   long duration, distance;
 
   // Clear the trigger pin
@@ -29,20 +33,31 @@ void loop() {
   distance = (duration / 2) / 29.1;
 
   // Check if the calculated distance is less than 10 cm
-  if (distance < 10) {
-    // If yes, turn on the LED
-    digitalWrite(led, HIGH);
-  } else {
-    // If no, turn off the LED
-    digitalWrite(led, LOW);
-  }
+  // if (distance < 10) {
+  //   // If yes, turn on the LED
+  //   digitalWrite(led, HIGH);
+  // } else {
+  //   // If no, turn off the LED
+  //   digitalWrite(led, LOW);
+  // }
+
 
   // Print the calculated distance to the Serial Monitor
   Serial.print(distance);
   Serial.println(" cm");
 
+   // Control the LED based on distance
+  if (distance <= 0) {
+    analogWrite(led, 0);  // Turn off the LED if there is no object
+  } else {
+    // Map the distance to a brightness value between 0 and 255 (reverse mapping)
+    brightness = map(distance, 0, 100, 255, 0);
+    brightness = constrain(brightness, 0, 255);  // Ensure brightness is within valid range
+    analogWrite(led, brightness);
+  }
+
   // Delay for 500 milliseconds before the next iteration
-  delay(500);
+  delay(1000);
 }
 
 
